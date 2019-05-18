fb9ff6edb4f9bb3605ae5c35147dfbc9bdeafa27
chengxiaonan <xiaonan.cheng@e-jiazi.com>
2019-05-18 10:56:58 +0800

    ssm提交

--- src/main/java/com/core/dao/UserDao.java
+++ src/main/java/com/core/dao/UserDao.java
@@ -1,10 +1,12 @@
 package com.core.dao;
 
 import com.core.entity.User;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by chengxiaonan on 2019/5/18.
  */
+@Repository
 public interface UserDao {
 
     public User login(User user);
--- src/main/java/com/core/service/impl/UserServiceImpl.java
+++ src/main/java/com/core/service/impl/UserServiceImpl.java
@@ -1,15 +1,22 @@
 package com.core.service.impl;
 
+import com.core.dao.UserDao;
 import com.core.entity.User;
 import com.core.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by chengxiaonan on 2019/5/18.
  */
+@Service("userService")
 public class UserServiceImpl implements UserService {
+    @Resource
+    private UserDao userDao;
+
     @Override
     public User login(User user) {
-
-        return user;
+        return userDao.login(user);
     }
 }
